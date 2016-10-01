2ece0e9434941e89add9ca3397847f9c283693a8
Gabor Vecsei <vecseigabor.x@gmail.com>
2016-10-02 01:55:07 +0200

    comments added

--- pyTRON.py
+++ pyTRON.py
@@ -27,8 +27,10 @@ class Court():
 		self.court_width = width
 		self.court_height = height
 		self.court = []
+		# Prepare court for usage
 		self.build()
 
+	# Prepare the court (draw the edges etc...)
 	def build(self):
 		# Build the court
 		for i in range(self.court_height):
@@ -47,6 +49,8 @@ class Court():
 			if len(court_row) > 0:
 				self.court.append(court_row)
 
+
+	# Print the court on the console
 	def printCourt(self):
 		cls()
 		# Print the court
@@ -57,9 +61,11 @@ class Court():
 				sys.stdout.write(self.court[i][k])
 				sys.stdout.flush()
 
+	# Put a character somewhere in the court
 	def buildOnCourt(self, x, y, char):
 		self.court[y][x] = char
 
+	# Draw the players with their given character
 	def drawPlayers(self, players, playerCharacters):
 		# Check if we has the same number of players as characters
 		if len(players) != len(playerCharacters):
@@ -70,11 +76,10 @@ class Court():
 				for pos in player.getPositions():
 					self.buildOnCourt(pos[0], pos[1], playerCharacters[i])
 
+	# Generate a random position within the court
 	def generateRandomPositionOnCourt(self):
 		x, y = np.random.randint(1,self.court_width-2), np.random.randint(1,self.court_height-2)
 		return (x,y)
-
-
 
 
 class Player():
@@ -83,10 +88,11 @@ class Player():
 		self.currDir = currDir
 		self.positions = []
 
-	# Stores the (x,y) positions in an array
+	# Stores the given (x,y) positions in an array
 	def storePosition(self, position):
 		self.positions.append(position)
 
+	# Returns the position array where we can see the players positions
 	def getPositions(self):
 		return self.positions
 
@@ -120,15 +126,17 @@ class Player():
 			self.storePosition(newPosition)
 			return True
 
+	# Detects if the player collided with itself
 	def detectSelfCollision(self, nextPosition):
 		if nextPosition in self.positions:
 			return True
 		else:
 			return False
 
-
+# Create a new court
 court = Court(100, 20)
 
+# Create a player
 player_one_start_pos = court.generateRandomPositionOnCourt()
 player_one = Player()
 player_one.storePosition(player_one_start_pos)
