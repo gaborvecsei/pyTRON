a6fc85f8c5ac970d295f117fe890f88542f62c33
Gabor Vecsei <vecseigabor.x@gmail.com>
2016-10-01 21:25:11 +0200

    player class

--- pyTRON.py
+++ pyTRON.py
@@ -43,14 +43,49 @@ def cls():
 # Show court for first time
 showCourt(COURT)
 
-player_one_pos = []
+def generateRandomPosition():
+	x, y = np.random.randint(1,COURT_WIDTH-2), np.random.randint(1,COURT_HEIGHT-2)
+	return (x,y)
 
-player_one_start_pos_x, player_one_start_pos_y = np.random.randint(1,COURT_WIDTH-2), np.random.randint(1,COURT_HEIGHT-2)
-COURT[player_one_start_pos_y][player_one_start_pos_x] = 'Q'
+def buildOnCourt(x,y,char):
+	COURT[y][x] = char
 
-# This is the starting position for player one
-player_one_pos.append((player_one_start_pos_x, player_one_start_pos_y))
+class Player():
+
+	def __init__(self):
+		self.currDir = 0
+		self.positions = []
+
+	# Stores the (x,y) positions in an array
+	def storePosition(self, position):
+		self.positions.append(position)
+
+	def getPositions(self):
+		return self.positions
+
+	# Set the current direction
+	def setCurrentDirection(self, dir):
+		self.currDir = dir
+
+	def getcurrentDirection(self):
+		return self.currDir
+
+
+player_one_start_pos = generateRandomPosition()
+player_one = Player()
+player_one.storePosition(player_one_start_pos)
+
+for pos in player_one.getPositions():
+	buildOnCourt(pos[0], pos[1], 'o')
 
 # Show player
 showCourt(COURT)
 
+# fps = 5
+# time_delta = 1./fps
+
+# while True:
+#     t0 = time.clock()
+#     time.sleep(time_delta)
+#     t1 = time.clock()
+#     print 1. / (t1 - t0)
