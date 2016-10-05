ee69a374888949d25671a222e859c380a2f3cb9e
Gabor Vecsei <vecseigabor.x@gmail.com>
2016-10-05 18:29:23 +0200

    menu added and player improvements

--- pyTRON.py
+++ pyTRON.py
@@ -1,3 +1,15 @@
+"""
+*****************************************************
+*
+*              Gabor Vecsei
+* Email:       vecseigabor.x@gmail.com
+* Blog:        https://gaborvecsei.wordpress.com/
+* LinkedIn:    www.linkedin.com/in/vecsei-gabor
+* Github:      https://github.com/gaborvecsei
+*
+*****************************************************
+"""
+
 import time
 import os
 import sys
@@ -5,20 +17,7 @@ import numpy as np
 # Windows OS specific:
 import msvcrt
 
-# Clears the console's screen
-def cls():
-	os.system('cls' if os.name=='nt' else 'clear')
-
-# Asks whether a key has been acquired
-def kbfunc():
-	x = msvcrt.kbhit()
-	if x:
-		ret = msvcrt.getch()
-	else:
-		ret = False
-	return ret
-
-
+# This is a class that represents the game's court
 class Court():
 
 	def __init__(self, width, height):
@@ -81,12 +80,23 @@ class Court():
 		return (self.court_width, self.court_height)
 
 
+
+# This is a class for the players
+
 class Player():
 
-	def __init__(self, currDir = 0, playerCharacter = 'o'):
+	# inputCharacters: you have to give in this order: (left, right, up, down)
+	# These are the characters you can controll your player
+	# playerCharacter: This is the character we will draw on the court to display the player
+	def __init__(self, startPosition, inputCharacters, currDir = 0, playerCharacter = 'o'):
 		self.currDir = currDir
 		self.positions = []
 		self.playerCharacter = playerCharacter
+		self.inputCharacters = inputCharacters
+		if len(self.inputCharacters) != 4:
+			print "Error with player input characters!"
+			sys.exit(1)
+		self.storePosition(startPosition)
 
 	# Stores the given (x,y) positions in an array
 	def storePosition(self, position):
@@ -148,62 +158,176 @@ class Player():
 		else:
 			return False
 
+	# Detect collision with other players
+	def detectPlayerCollision(self, otherPlayers):
+		lastPosition = self.positions[-1]
+		for otherPlayer in otherPlayers:
+			if lastPosition in otherPlayer.getPositions():
+				# Return the collision position too (we can draw it etc..., it's more user friendly)
+				return True, lastPosition
+			else:
+				return False, (0,0)
+		return False, (0,0)
 
 
-def ControllPlayer(keyPressed, player, inputChars):
-	left, right, up, down = inputChars
-	# LEFT
-	if keyPressed != False and keyPressed.decode() == left and player.getcurrentDirection() != 1:
-		player.changeDirection(0)
-		# You can use:  player_one.changeDirection(player_one.translateDirection('left')) too
-	# RIGHT
-	elif keyPressed != False and keyPressed.decode() == right and player.getcurrentDirection() != 0:
-		player.changeDirection(1)
-	# UP
-	elif keyPressed != False and keyPressed.decode() == up and player.getcurrentDirection() != 3:
-		player.changeDirection(2)
-	# DOWN
-	elif keyPressed != False and keyPressed.decode() == down and player.getcurrentDirection() != 2:
-		player.changeDirection(3)
+	# Change the player's direction based on the user input
+	def controllPlayerWithInput(self, keyPressed):
+		left, right, up, down = self.inputCharacters
+
+		# LEFT
+		if keyPressed != False and keyPressed.decode() == left and self.getcurrentDirection() != 1:
+			self.changeDirection(0)
+			# You can use:  player_one.changeDirection(player_one.translateDirection('left')) too
+		# RIGHT
+		elif keyPressed != False and keyPressed.decode() == right and self.getcurrentDirection() != 0:
+			self.changeDirection(1)
+		# UP
+		elif keyPressed != False and keyPressed.decode() == up and self.getcurrentDirection() != 3:
+			self.changeDirection(2)
+		# DOWN
+		elif keyPressed != False and keyPressed.decode() == down and self.getcurrentDirection() != 2:
+			self.changeDirection(3)
 
 
-# Create a new court
-court = Court(100, 20)
+# Clears the console's screen
+def cls():
+	os.system('cls' if os.name=='nt' else 'clear')
 
-# Create a player
-player_one_start_pos = court.generateRandomPositionOnCourt()
-player_one = Player(playerCharacter='x')
-player_one.storePosition(player_one_start_pos)
+# Asks whether a key has been acquired
+def kbfunc():
+	x = msvcrt.kbhit()
+	if x:
+		ret = msvcrt.getch()
+	else:
+		ret = False
+	return ret
 
-# Create a player
-player_two_start_pos = court.generateRandomPositionOnCourt()
-player_two = Player(playerCharacter='o')
-player_two.storePosition(player_two_start_pos)
+# Goes to the main menu if the user presses x
+def gotoMainMenu():
+	print 'Press (x) to go back to the main menu'
+	pressedKey = raw_input('')
+	if pressedKey == 'x' or pressedKey == 'X':
+		cls()
+		mainScreen()
+	else:
+		gotoMainMenu()
 
-fps = 30
-time_delta = 1./fps
+# We start the game with this screen where we can choose what to do
+def mainScreen():
+	cls()
+	print "Welcome to pyTRON!"
+	print "\nTron game written in python by Gabor Vecsei\n"
+	print "Press (1) to play a game"
+	print "Press (2) to see who made it"
+	print "Press (3) to show some help"
+	print "Press (4) to exit from the game"
+	
+	selectedMenuPoint = raw_input("Enter the selected menu point and press ENTER: ")
+	print "\n\n"
 
-while True:
-	t0 = time.clock()
-	time.sleep(time_delta)
-	t1 = time.clock()
+	if eval(selectedMenuPoint) == 1:
+		gamePlayScreen()
+	elif eval(selectedMenuPoint) == 2:
+		aboutScreen()
+	elif eval(selectedMenuPoint) == 3:
+		helpScreen()
+	elif eval(selectedMenuPoint) == 4:
+		exit()
 
-	# Detect key hits
-	keyPressed = kbfunc()
-	if keyPressed != False:
-		# Change player directions if necessary
-		ControllPlayer(keyPressed, player_one, ('a', 'd', 'w', 's'))
-		ControllPlayer(keyPressed, player_two, ('j', 'l', 'i', 'k'))
+def aboutScreen():
+	cls()
+	print "		Tron game made by Gabor Vecsei"
+	print "		2016 October"
+	print """
+		/*****************************************************
+		*
+		*              Gabor Vecsei
+		* Email:       vecseigabor.x@gmail.com
+		* Blog:        https://gaborvecsei.wordpress.com/
+		* LinkedIn:    www.linkedin.com/in/vecsei-gabor
+		* Github:      https://github.com/gaborvecsei
+		*
+		*****************************************************
+		"""
+	gotoMainMenu()
 
-	for player in [player_one, player_two]:
-		player.moveOneStep()
-		if player.detectSelfCollision() == True:
-			print "\nSELF COLLISION for player: " + player.getCharacter()
-			sys.exit(0)
-		if player.detectCourtCollision(court) == True:
-			print "\nWALL COLLISION for player: " + pilayer.getCharacter()
-			sys.exit(0)
+def helpScreen():
+	cls()
+	print "Help menu, so you can play pyTRON\n"
+	print "Okay, okay...I will help you, just read this:"
+	print "Avoid collisions with the wall, other players and even with yourself."
+	print "That's it...Start playing! :P"
+	gotoMainMenu()
 
-	# Show the court and players
-	court.drawPlayers([player_one, player_two])
-	court.printCourt()
+def exit():
+	print "\nThank you for playing!\n"
+	sys.exit(0)
+
+def gamePlayScreen(fps = 20):
+
+	# Build a court
+	court = Court(100, 20)
+	
+	# Create a player
+	player_one_start_pos = court.generateRandomPositionOnCourt()
+	player_one = Player(player_one_start_pos, ('a', 'd', 'w', 's'), playerCharacter='x')
+	# Move in a random direction at first
+	player_one.changeDirection(np.random.randint(0,4))
+	
+	# Create a player
+	player_two_start_pos = court.generateRandomPositionOnCourt()
+	player_two = Player(player_two_start_pos, ('j', 'l', 'i', 'k'), playerCharacter='o')
+	# Move in a random direction at first
+	player_two.changeDirection(np.random.randint(0,4))
+
+	players = [player_one, player_two]
+
+	time_delta = 1./fps
+	
+	while True:
+		t0 = time.clock()
+		time.sleep(time_delta)
+		t1 = time.clock()
+
+		# Detect user input
+		keyPressed = kbfunc()
+	
+		# Move the players and detect collisions
+		for player in players:
+			# For inputs change the players direction
+			if keyPressed != False:
+				player.controllPlayerWithInput(keyPressed)
+
+			# Move the player
+			player.moveOneStep()
+
+			if player.detectSelfCollision() == True:
+				print "\nSELF COLLISION for player: " + player.getCharacter()
+				gotoMainMenu()
+	
+			if player.detectCourtCollision(court) == True:
+				print "\nWALL COLLISION for player: " + player.getCharacter()
+				gotoMainMenu()
+	
+			# Copy the players array
+			otherPlayers = players[:]
+			# Remove the "active" player and check if the activa player collided it with someone
+			otherPlayers.remove(player)
+			isPlayerCollision, collisionPosition = player.detectPlayerCollision(otherPlayers)
+			if isPlayerCollision:
+				# Put a character where the collision happened
+				court.buildOnCourt(collisionPosition[0], collisionPosition[1], '#')
+				# Draw the court one last time to show the collision
+				court.printCourt()
+				print "\nPlayers Collision for player: " + player.getCharacter()
+				gotoMainMenu()
+
+			# Show the court and players in the console
+			court.drawPlayers([player_one, player_two])
+			court.printCourt()
+
+
+############ HERE STARTS THE GAME ################ 
+
+# Show the main screen
+mainScreen()
